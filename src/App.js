9d22bc7002acdecb3adc601e2ba6207c0bc70de3
AJ Speller <ajspeller@gmail.com>
2019-05-03 11:40:36 -0400

    Create todo list and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { id: 1, content: `Keep studying don't give up` },
+        { id: 2, content: `Learn node.js` },
+        { id: 3, content: `Learn React` },
+        { id: 4, content: `Learn Angular` },
+        { id: 5, content: `Learn Express` },
+        { id: 6, content: `Learn CSS/Bootstrap/Materialize` },
+        { id: 7, content: `Learn MongoDB` }
+      ]
+    };
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  deleteTodo(id) {
+    console.log(`deleting todo`);
+    console.log(this.state.todos.filter((t) => id === t.id));
+    const todos = this.state.todos.filter((t) => id !== t.id);
+    this.setState({
+      todos: todos
+    });
+  }
+
+  render() {
+    return (
+      <div className='App container'>
+        <h1 className='center blue-text'>Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Todos.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Todos = ({ todos, deleteTodo }) => {
+  const todoList = todos.length ? (
+    todos.map((t) => {
+      return (
+        <div
+          key={t.id}
+          className='collection-item'
+          onClick={() => {
+            deleteTodo(t.id);
+          }}
+        >
+          <span>{t.content}</span>
+        </div>
+      );
+    })
+  ) : (
+    <p className='center'>You have completed all of your todos! Yeahhh baby!</p>
+  );
+  return <div className='Todos collection'>{todoList}</div>;
+};
+
+export default Todos;
